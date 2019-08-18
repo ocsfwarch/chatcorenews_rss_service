ca04d4cb6caed836e2ebe9fdd5c91c07fb74abec
ocsfwarch <webmaster@ocsfwarch.com>
2019-08-18 16:04:42 -0700

    Update for async/await additions

--- src/components/OcsaDbServices.js
+++ src/components/OcsaDbServices.js
@@ -9,35 +9,85 @@ class OcsaDbServices{
 
     openDbConnection(){
         if(this.dbConn === null){
-        this.dbConn = mysql.createConnection({
-            host:     'localhost',
-            user:     'ocsfwarch',
-            password: 'TP2013bb',
-            database: 'chat_news'
-        });   
-        // Connect to database
-        this.dbConn.connect();
-        return true; 
+            this.dbConn = mysql.createConnection({
+                host:     'localhost',
+                user:     'ocsfwarch',
+                password: 'TP2013bb',
+                //database: 'chat_news'
+                database: 'chat_news_test'
+            });   
+            // Connect to database
+            this.dbConn.connect();
+            return true; 
+        }else{
+            return false;
         }
     }
 
     closeDbConnection(){
         if(this.dbConn !== null){
             this.dbConn.end();
+            this.dbConn = null;
         }
+        return false;
+    }
+
+
+    readValueFromDb(strKey,strQuery){
+        return new Promise((resolve,reject) => {
+          //this.dbConn.query('SELECT nFeedId, strFeedName AS name,strFeedValue AS value,strFeedCategory AS category, nAllowLookup FROM feedlist_table WHERE nActive = 1 ORDER BY category, name',
+            if(this.dbConn !== null){
+                this.dbConn.query(strQuery,
+                (error, results, fields) => {
+                    if(error){
+                    return reject(`ERROR - readFeedList Failed due to ${error}` );
+                    }
+                    //var rows = JSON.parse(JSON.stringify(results[0]));
+                    //console.log(`rows = ${results[0].nFeedId}`);
+                    if(results.length > 0){
+                        return resolve(true);
+                    }else{
+                        return resolve(false);
+                    }
+                });
+            }else{
+                return reject(`ERROR:readFromDb = dbConn is not open`);
+            }
+        });
     }
 
     readFromDb(strQuery){
         return new Promise((resolve,reject) => {
           //this.dbConn.query('SELECT nFeedId, strFeedName AS name,strFeedValue AS value,strFeedCategory AS category, nAllowLookup FROM feedlist_table WHERE nActive = 1 ORDER BY category, name',
-          this.dbConn.query(strQuery,
-          (error, results, fields) => {
-              if(error){
-               return reject("ERROR - readFeedList Failed");
-              }
-              this.setJsonObj(JSON.stringify(results));
-              return resolve("SUCCESS - readFeedList Success");
-          });
+            if(this.dbConn !== null){
+                this.dbConn.query(strQuery,
+                (error, results, fields) => {
+                    if(error){
+                    return reject(`ERROR - readFeedList Failed due to ${error}` );
+                    }
+                    this.setJsonObj(JSON.stringify(results));
+                    return resolve(true);
+                });
+            }else{
+                return reject(`ERROR:readFromDb = dbConn is not open`);
+            }
+        });
+    }
+
+    async writeToDb(strQuery){
+        return new Promise((resolve,reject) => {
+            if(this.dbConn !== null){
+            this.dbConn.query(strQuery,
+            (error, results, fields) => {
+                if(error){
+                return reject(`ERROR - writeToDbFailed, ${error}`);
+                }
+                //this.setJsonObj(JSON.stringify(results));
+                return resolve(true);
+                });
+            }else{
+                return reject("ERROR:writeToDb = dbConn is not open");
+            }
         });
     }
 
--- src/server.js
+++ src/server.js
@@ -11,95 +11,125 @@ class OcsaRssReader{
 
   constructor(){
     this.bDbConnValid   = false;
-    this.feedList       = [];
+    this.feedList       = null;
     this.bFeedListValid = false;
     this.nTotalFeeds    = 0;
     this.nTotalErrors   = 0;
     this.nTotalSuccess   = 0;
   }
 
-  runRssService(){
-    ocsaLogger.writeToLog("START = runRssService");
-    this.bDbConnValid = ocsaDb.openDbConnection();
-    if(this.bDbConnValid){
-      ocsaLogger.writeToLog("Success Opening Db Conn");
-      const strSqlQuery = 'SELECT nFeedId, strFeedName AS name,strFeedValue AS value,strFeedCategory AS category, nAllowLookup FROM feedlist_table WHERE nActive = 1 ';
-      ocsaDb.readFromDb(strSqlQuery)
-        .then(result => {
-          ocsaDb.closeDbConnection();
-          ocsaLogger.writeToLog(`Results = ${result}`);
-          let objJson = JSON.parse(ocsaDb.getJsonObj());
-          objJson.forEach((feedItem) => {
-              let arrayFeed     = feedItem.value.split(" ");
-              const strFeedName = arrayFeed[1];
-              const strFeedUrl  = arrayFeed[0];
-              const nFeedId     = feedItem.nFeedId;
-              //const nTitleHash  = 0;
-              ocsaLogger.writeToLog(`nFeedId = ${nFeedId}`);
-              (async() => {
-                 //let feed = await parser.parseURL('https://www.reddit.com/.rss');
-                 let feed = await parser.parseURL(strFeedUrl);
-                 ocsaLogger.writeToLog(feed.title);
-                 let feedItems = [];
-                 feed.items.forEach(entry => {
-                      const feedItem = {
-                       nFeedId:nFeedId,
-                       nTitleHash:hash.hashCode(entry.title.replace(/\s/g,'').toLowerCase()),
-                       strTitle:entry.title,
-                       strOrgTitle:entry.title,
-                       strOrgUri:entry.link,
-                       strLinkUri:entry.link,
-                       strImageUri:'',
-                       dtDateTime:'2019-08-14 03:04:52',
-                       strDateTime:'August-14-2019, 3pm',
-                       strDescription:entry.contentSnippet,
-                       strCategory:'NEWS',
-                       strFeedLink:feed.link,
-                     }
-                     //const strTitle   = entry.title;
-                     //const nTitleHash = hash.hashCode(entry.title.replace(/\s/g,'').toLowerCase());
-                     //ocsaLogger.writeToLog(`nFeedId = ${feedItem.nFeedId}`);
-                     //ocsaLogger.writeToLog(`nTitleHash = ${feedItem.nTitleHash}`);
-                     //ocsaLogger.writeToLog(`strTitle = ${feedItem.strTitle}`);
-                     ocsaLogger.writeToLog(`strFeedLink = ${feedItem.strFeedLink}`);
-                     feedItems.push(feedItem);
-                 });
-                 ocsaLogger.writeToLog(`FeedItems = ${feedItems.length}`);
-                 return feedItems;
-              })();
-          });
-        })        
-        .catch(err => {ocsaLogger.writeToLog(`ERROR: ${err}`);ocsaDb.closeDbConnection();});
+  async parseFeedItems(nFeedId, theFeed){
+    let bReturn = false;
+    const nLen = theFeed.items.length;
+    let nCounter = 0;
+    try{
+      ocsaLogger.writeToLog(`Total Items = ${theFeed.items.length}`);
+      let nIndex = 0;
+      let bResults = false;
+      for(nIndex; nIndex < nLen; nIndex++){
+        const entry = theFeed.items[nIndex];        
+        const feedItem = {
+          nFeedId:nFeedId,
+          nTitleHash:hash.hashCode(entry.title.replace(/\s/g,'').toLowerCase()),
+          strTitle:entry.title.replace(/'/g,"''"),
+          strOrgUri:entry.link.replace(/'/g,"''"),
+          strLinkUri:entry.link,
+          strImageUri:'',
+          strDateTime:'August-14-2019, 3pm',
+          strDescription:entry.contentSnippet.replace(/'/g,"''"),
+          strCategory:'NEWS',
+          strFeedLink:theFeed.link
+        }
+        const strDupQueryCheck = `SELECT nFeedId FROM feedresults_table WHERE nFeedId = ${feedItem.nFeedId} AND nTitleHash = ${feedItem.nTitleHash}`;
+        bResults = await ocsaDb.readValueFromDb('nFeedId', strDupQueryCheck);
+        if(!bResults){
+          const strQuery = `INSERT INTO feedresults_table(nFeedId, nTitleHash, strTitle, strOrgUri, strLinkUri, strImageUri, strDateTime, strDescription,strCategory,strFeedLink)VALUES(${feedItem.nFeedId},${feedItem.nTitleHash},\'${feedItem.strTitle}\',\'${feedItem.strOrgUri}\',\'${feedItem.strLinkUri}\',\'${feedItem.strImageUri}\',\'${feedItem.strDateTime}\',\'${feedItem.strDescription}\',\'${feedItem.strCategory}\',\'${feedItem.strFeedLink}\')`;
+          bResults = await ocsaDb.writeToDb(strQuery);
+          if(bResults){
+            nCounter++;
+            bResults = false;
+          }
+        }// End if(!bResults)
+      }// End for()
+    }catch(err){
+        ocsaLogger.writeToLog(`parseFeedItem Error: ${err}`);
+        return err;
     }
-    return false;
+    ocsaLogger.writeToLog(`Total = ${nLen}, Counted = ${nCounter}`);
+    return bReturn;
+  }// End parseFeedItem()
+
+  async parseFeedList(){
+    let bReturn = false;
+    try{
+      ocsaLogger.writeToLog(`Opening Database...`);
+      this.bDbConnValid = ocsaDb.openDbConnection();
+      if(this.bDbConnValid){
+        const nLen = this.feedList.length;
+        let nIndex = 0;
+        for(nIndex; nIndex < nLen; nIndex++){
+          const nFeedId    = this.feedList[nIndex].nFeedId;
+          const strFeedUrl = this.feedList[nIndex].value;
+          ocsaLogger.writeToLog(`nFeedId = ${nFeedId}, Url = ${strFeedUrl}`);
+          let feed = await parser.parseURL(strFeedUrl);
+          await this.parseFeedItems(nFeedId, feed);
+        }// End for()
+        ocsaLogger.writeToLog(`Closing Database...`);
+        this.bDbConnValid = ocsaDb.closeDbConnection();
+      }// End if(this.bDbConnValid)
+    }catch(err){
+      ocsaLogger.writeToLog(`parseFeedList Error: ${err}`);
+      ocsaLogger.writeToLog(`Closing Database...`);
+      this.bDbConnValid = ocsaDb.closeDbConnection();
+    }
+    return bReturn;
+  }// End parseFeedList()
+
+  async getFeedList(){
+    let bReturn = false;
+    try{
+      ocsaLogger.writeToLog(`Opening Database...`);
+      this.bDbConnValid = ocsaDb.openDbConnection();
+      if(this.bDbConnValid){
+        ocsaLogger.writeToLog("Success Opening Db Conn");
+        const strSqlQuery = 'SELECT nFeedId, strFeedTitle as title, strFeedName AS name,strFeedUrl AS value,strFeedCategory AS category, nAllowLookup FROM feedlist_table WHERE nActive = 1 ';
+        let dbResults = await ocsaDb.readFromDb(strSqlQuery);
+        if(dbResults){
+          this.feedList = JSON.parse(ocsaDb.getJsonObj());
+          ocsaLogger.writeToLog(`Size = ${this.feedList.length}`);
+          bReturn = true;
+        }
+        this.bDbConnValid = ocsaDb.closeDbConnection();
+        ocsaLogger.writeToLog(`Closing Database...`);
+      }else{
+        ocsaLogger.writeToLog("ERROR - Failed open database");
+      }
+    }catch(err){
+      ocsaLogger.writeToLog(err);
+    }// End catch
+    return bReturn;
+  }// End getFeedList()
+
+  async runFeedUpdate(){
+    let bReturn = false;
+    try{
+      bReturn = await this.getFeedList();
+      if(bReturn){
+        ocsaLogger.writeToLog(`Starting parseFeedList`);
+        bReturn = await this.parseFeedList();
+      }
+    }catch(err){
+      ocsaLogger.writeToLog(err);
+    }
+    return bReturn;
   }
-}
+
+  runRssService(){
+    this.runFeedUpdate().then(
+      result => {ocsaLogger.writeToLog(`Final Results = ${result}`);}
+    ).catch(err => {ocsaLogger.writeToLog(`${err}`);});
+  }
+}// End class OcsaRssReader
 
 let theReader = new OcsaRssReader();
-//theReader.writeToLog('test');
 theReader.runRssService();
-console.log("DONE");
-
-
-
-
-//function testRSS(){
-//    parser.parseURL('http://www.realtruenews.org/feed.xml', function(err, feed) {
-//        console.log(`Feed Size = ${feed.items.length}`);
-//        feed.items.forEach(function(entry) {
-//          console.log(entry.title + ':' + entry.link);
-//          console.log('*******************************');
-//        });
-//      });
-//}// End of testRSS
-
-//console.log("Ready to start testRSS");
-//testRSS();
-//if(readFeedList()){
-//  console.log("Got the FeedList");
-//  console.log(feedList[data].length);
-//}else{
-//  console.log("Error getting FeedList");
-//}
-
-//dbConn.end();
